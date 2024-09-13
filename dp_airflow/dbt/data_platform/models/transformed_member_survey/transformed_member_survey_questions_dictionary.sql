13f2ed19e195295e50e0960b14a6624064e878b7
Sindy Lee <71305813+sindyl-loyalty@users.noreply.github.com>
2024-09-13 16:27:40 +1200

    Create transformed_member_survey_questions_dictionary.sql

---
+++ dp_airflow/dbt/data_platform/models/transformed_member_survey/transformed_member_survey_questions_dictionary.sql
@@ -0,0 +1,27 @@
+{{
+    config(
+        alias='member_survey_questions_dictionary',        
+        materialized='incremental',
+        incremental_strategy='append',
+        tags = ["loyaltyHub_migration"],     
+        schema = 'transformed_surveys'
+    )
+}}
+
+
+SELECT
+    index ,
+    field_name ,
+    section ,
+    question ,
+    sub_question ,
+    question_description ,
+    sub_question_description ,
+    data_type ,
+    survey_id ,
+    DATE(load_date_nzt) AS load_date_nzt
+FROM
+    {{ ref("derived_member_survey_questions_dictionary") }}
+{% if is_incremental() %}
+  where DATE(load_date_nzt) > (select max(load_date_nzt) from {{ this }})
+{% endif %}
