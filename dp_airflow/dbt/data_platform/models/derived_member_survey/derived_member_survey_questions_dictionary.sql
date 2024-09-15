6215306de7c51d077b232fef3e902cda115c91c1
Sindy Lee <71305813+sindyl-loyalty@users.noreply.github.com>
2024-09-16 11:17:26 +1200

    Create derived_member_survey_questions_dictionary.sql

---
+++ dp_airflow/dbt/data_platform/models/derived_member_survey/derived_member_survey_questions_dictionary.sql
@@ -0,0 +1,26 @@
+{{
+    config(
+        alias='member_survey_questions_dictionary',        
+        materialized='incremental',
+        incremental_strategy='append',        
+        schema = 'derived_surveys'
+    )
+}}
+
+
+SELECT
+    index ,
+    field_name ,
+    section ,
+    question ,
+    sub_question ,
+    question_description ,
+    sub_question_description ,
+    data_type ,
+    survey_id ,    
+    load_date_nzt
+FROM
+    {{ ref("raw_member_survey_questions_dictionary") }}
+{% if is_incremental() %}
+  where DATE(load_date_nzt) > (select max(DATE(load_date_nzt)) from {{ this }})
+{% endif %}
